f10b405d17935726c911d73e7f3262fd000728ac
xuruidong <xuruidong@gmail.com>
2020-08-07 18:31:18 +0800

    smemory

--- python-3/python-3.py
+++ python-3/python-3.py
@@ -176,20 +176,32 @@ def pipe_test():
 
 from multiprocessing import Value
 from multiprocessing import Array
+from ctypes import c_char_p
 
-def f_sharememory(n, a):
+def f_sharememory(n, a, s):
+    print ("=======111")
+    sys.stdout.flush()
     n.value = 3.1415927
     for i in a:
         a[i] = -a[i]
+    
+    print ("=======")
+    sys.stdout.flush()
+    #print (dir(s.value))
+    sys.stdout.flush()
+    #print (type(s.value))
+    print ("-------------")
+    sys.stdout.flush()
 
 def sharememory_test():
     num = Value('d', 0.0)
     arr = Array('i', range(10))
+    string = Value(c_char_p, b"Hello World")
 
     print(num.value)
     print(arr[:])
     
-    p = Process(target=f_sharememory, args=(num, arr))
+    p = Process(target=f_sharememory, args=(num, arr, string))
     p.start()
     p.join()
 
