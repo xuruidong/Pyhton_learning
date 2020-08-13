e2f05c94b1a1d9f06e978820a5c7cb8555a15bd9
xuruidong <xuruidong@gmail.com>
2020-08-13 18:25:55 +0800

    xxx

--- python-4/python-4.py
+++ python-4/python-4.py
@@ -138,7 +138,8 @@ def pandas_adjust_test():
     df.T.T    
     
     
-    
+def xxx():
+    [{'a': 1, 'b': 2, 'c': 3, 'd': 4,}, {'a': 5, 'b': 6, 'c': 7, 'd': 8,}]
     
 if __name__ == "__main__":
     # sklearn_test()
